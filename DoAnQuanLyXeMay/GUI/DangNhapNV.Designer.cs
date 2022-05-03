91e5ba2d07e0bccc26da276fa2832dd29253e2f1
Dat76 <102209716+Dat76@users.noreply.github.com>
2022-05-03 22:19:44 +0700

    nua ne

---
+++ DoAnQuanLyXeMay/GUI/DangNhapNV.Designer.cs
@@ -0,0 +1,139 @@
+﻿
+namespace DoAnQuanLyXeMay
+{
+    partial class DangNhapNV
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_mknv = new System.Windows.Forms.TextBox();
+            this.btn_thoat = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_tknv = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_dnnv = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Museo 700", 20.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.label3.Location = new System.Drawing.Point(132, 42);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(176, 32);
+            this.label3.TabIndex = 20;
+            this.label3.Text = "ĐĂNG NHẬP";
+            // 
+            // txt_mknv
+            // 
+            this.txt_mknv.Location = new System.Drawing.Point(184, 212);
+            this.txt_mknv.Name = "txt_mknv";
+            this.txt_mknv.Size = new System.Drawing.Size(194, 20);
+            this.txt_mknv.TabIndex = 19;
+            // 
+            // btn_thoat
+            // 
+            this.btn_thoat.Font = new System.Drawing.Font("Museo 700", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_thoat.ForeColor = System.Drawing.Color.MediumVioletRed;
+            this.btn_thoat.Location = new System.Drawing.Point(270, 297);
+            this.btn_thoat.Name = "btn_thoat";
+            this.btn_thoat.Size = new System.Drawing.Size(94, 44);
+            this.btn_thoat.TabIndex = 18;
+            this.btn_thoat.Text = "Thoát";
+            this.btn_thoat.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Modern No. 20", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.DarkOrchid;
+            this.label2.Location = new System.Drawing.Point(58, 212);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 21);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "Mật Khẩu";
+            // 
+            // txt_tknv
+            // 
+            this.txt_tknv.Location = new System.Drawing.Point(184, 130);
+            this.txt_tknv.Name = "txt_tknv";
+            this.txt_tknv.Size = new System.Drawing.Size(194, 20);
+            this.txt_tknv.TabIndex = 16;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Modern No. 20", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.BlueViolet;
+            this.label1.Location = new System.Drawing.Point(58, 129);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 21);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Tài Khoản";
+            // 
+            // btn_dnnv
+            // 
+            this.btn_dnnv.Font = new System.Drawing.Font("Museo 700", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_dnnv.ForeColor = System.Drawing.Color.MediumVioletRed;
+            this.btn_dnnv.Location = new System.Drawing.Point(74, 297);
+            this.btn_dnnv.Name = "btn_dnnv";
+            this.btn_dnnv.Size = new System.Drawing.Size(125, 44);
+            this.btn_dnnv.TabIndex = 14;
+            this.btn_dnnv.Text = "Đăng Nhập";
+            this.btn_dnnv.UseVisualStyleBackColor = true;
+            // 
+            // DangNhapNV
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(437, 383);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txt_mknv);
+            this.Controls.Add(this.btn_thoat);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txt_tknv);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_dnnv);
+            this.Name = "DangNhapNV";
+            this.Text = "DangNhapNV";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_mknv;
+        private System.Windows.Forms.Button btn_thoat;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_tknv;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_dnnv;
+    }
+}
